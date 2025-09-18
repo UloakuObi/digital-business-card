48baac30e5ab08d8d1bd184503ab4c8f327322e4
UloakuObi <uloakuobi@yahoo.com>
2025-09-18 10:51:58 +0100

    Update project

---
+++ README.md
@@ -0,0 +1,2 @@
+# digital-business-card
+Scrimba solo project (React)
---
+++ components/Interests.jsx
@@ -0,0 +1,13 @@
+function Interests() {
+    return (
+        <>
+            <h3>Interests</h3>
+            <p>
+            Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. 
+            Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.
+            </p>
+        </>
+    )
+}
+
+export default Interests
